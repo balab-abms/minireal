cd2dd1ef7a5ddd6feb174f5a6408dbd45eafff54
Duguma <dugyeshi@gmail.com>
2024-10-25 22:27:17 +0900

    FEAT: added javacdoc formats

---
+++ src/main/java/org/balab/minireal/javadoc/javadoc_format.java
@@ -0,0 +1,54 @@
+package org.balab.minireal.javadoc;
+
+/**
+ * summary sentence about the class
+ *
+ * <p>
+ *     Paragraph of important points
+ *     <ul>
+ *         <li> item </li>
+ *     </ul>
+ *
+ *     <code> important point </code>
+ *
+ *     {@link }
+ * </p>
+ *
+ * @author Duguma Yeshitla
+ * @version %I%
+ * @see
+ * @since 1.0
+ */
+public class javadoc_format {
+
+    /**
+     * summary phrase about the field
+     */
+    int example_field;
+
+    /**
+     * summary sentence about the constructor
+     * <p>
+     *     Paragraph of important points
+     * </p>
+     *
+     * @param
+     * @see
+     * @since 1.0
+     */
+    public javadoc_format(){}
+
+    /**
+     * summary sentence about the method
+     * <p>
+     *      paragraph of important points
+     * </p>
+     *
+     * @param
+     * @return Boolean
+     * @throws
+     * @see
+     * @since 1.0
+     */
+    public Boolean example_method(int param_a) {}
+}
